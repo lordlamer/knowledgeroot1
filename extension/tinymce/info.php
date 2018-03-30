5020135b5ff8e57f1aa17440300bd70b651115cc
Frank Habermann <lordlamer@lordlamer.de>
2018-03-30 23:50:23 +0200

    * update to tinymce 4.7.9

--- extension/tinymce/info.php
+++ extension/tinymce/info.php
@@ -2,12 +2,11 @@
 $CONF = array (
 	"title" => "tinymce for knowledgeroot",
 	"description" => "tinymce for knowledgeroot",
-	"version" => "3.4.8",
-	"history" => "3.2.2.1;3.2.2.3;3.2.3;3.2.3.1;3.2.5;3.4.2;3.4.8",
+	"version" => "4.7.9",
+	"history" => "3.2.2.1;3.2.2.3;3.2.3;3.2.3.1;3.2.5;3.4.2;3.4.8;4.7.9",
 	"dependencies" => "",
 	"state" => "stable",
 	"author" => "Frank Habermann",
 	"author_email" => "lordlamer@lordlamer.de",
 	"author_company" => "LordLamer",
 );
-?>
